89da1204ed37c99b931daac150f5a337790bb2ef
Manish Koirala <mail@manishkoirala.xyz>
2025-03-13 15:21:33 +0545

    New update

--- .local/src/suckless-utils/dwmblocks/blocks.h
+++ .local/src/suckless-utils/dwmblocks/blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"󰃠 ", "echo $(brillo -G | cut -d'.' -f1)%",	0,		4},
 	{"󰕾 ", "echo $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}')%",	1,		5},
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"󰂄 ", "echo $(cat /sys/class/power_supply/BAT*/capacity)%",	30,		0},
+	{"󰂄 ", "echo $(cat /sys/class/power_supply/BAT*/capacity)% $(cat /sys/class/power_supply/BAT*/status)",	30,		0},
 	{" ", "date +'%b %d (%a) %H:%M:%S'",	1,		0},
 };
 
