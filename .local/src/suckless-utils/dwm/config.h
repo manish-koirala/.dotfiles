80a73d5959bac2a03e946f7d2f9bbb6a06dbb783
Manish Koirala <mail@manishkoirala.xyz>
2025-03-13 15:22:52 +0545

    Update

--- .local/src/suckless-utils/dwm/config.h
+++ .local/src/suckless-utils/dwm/config.h
@@ -3,8 +3,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int snap      = 28;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -12,26 +12,21 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=12" };
-static const char dmenufont[]       = "JetBrainsMonoNerdFont:size=12";
+static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=11" };
+static const char dmenufont[]       = "JetBrainsMonoNerdFont:size=11";
 static const char col_gray1[]       = "#2a3132";
-static const char col_gray2[]       = "#73605b";
-static const char col_gray3[]       = "#66a5ad";
-static const char col_gray4[]       = "#eDf4f2";
-static const char col_cyan[]        = "#005577";
-static const char col_cherry[]        = "#591923";
-static const char col_teal[]        = "#C4DFE6";
+static const char col_teal[]       = "#66a5ad";
+static const char col_snow[]       = "#eDf4f2";
 static const char col_charcoal[]        = "#101820";
-static const char col_blue[]        = "#375E97";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_charcoal },
-	[SchemeSel]  = { col_gray4, col_charcoal,  col_gray4 },
+	[SchemeNorm] = { col_teal, col_gray1, col_charcoal },
+	[SchemeSel]  = { col_snow, col_charcoal,  col_snow },
 };
 
 /* tagging */
-static const char *tags[] = { "term", "www", "3", "4", "5", "6", "7" };
+static const char *tags[] = { "", "", "", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -39,11 +34,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,        0,            0,           0 },
+	{ "Gimp",     NULL,       NULL,        0,            1,           0 },
+	{ "Alacritty",     NULL,       NULL,        1 << 0,            0,           0 },
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           0 },
 	{ "Zathura",  NULL,       NULL,       1 << 2,       0,           0 },
+	{ "Zotero",     NULL,       NULL,        1 << 2,            0,           0 },
+	{ "Cherrytree",  NULL,       NULL,       1 << 2,       0,           0 },
 	{ "PacketTracer",  NULL,       NULL,       0,        1,           0 },
-	{ "Virt-manager",  NULL,       NULL,       1 << 3,        0,           0 },
+	{ "Virt-manager",  NULL,       NULL,       0,        1,           0 },
 };
 
 /* layout(s) */
@@ -73,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_charcoal, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_teal, "-sb", col_charcoal, "-sf", col_snow, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 /*brightness controls */
